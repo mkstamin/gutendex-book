6d3f08b0a0e07adb582db7ef324158bc095cab51
mkstamin <mkstamincse@gmail.com>
2024-10-19 16:22:18 +0600

    feat(API): call API and store the data

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("Hello world!");
+// set variables
+let bookData;
+let loading = true;
+
+// API call and return data
+async function fetchBooks(pageNumber) {
+  try {
+    const response = await fetch(
+      `https://gutendex.com/books/?page=${pageNumber}`
+    );
+    const data = await response.json();
+    bookData = data.results;
+
+    loading = false;
+
+    return data.results;
+  } catch (error) {
+    console.log(error.message);
+    loading = false;
+
+    return [];
+  }
+}
+
+window.addEventListener("load", function () {
+  handleDisplayBooks(currentPage);
+});
